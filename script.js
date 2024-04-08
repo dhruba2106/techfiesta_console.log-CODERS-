2c20d7ca7118a2a90aad54294437a70ddc5cd80f
Dhruba Das <ddas96591@gmail.com>
2024-04-08 17:37:21 +0530

    clashde

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn1=document.querySelector("button");
+btn1.innerHTML="<i>CLICK!</i>";
+btn1.style.backgroundColor="red";
+btn1.onclick=()=>{
+ console.log("the price is seen ove r here");
+}
+
